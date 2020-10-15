4e6107542e904e0c10ac519db80551883cb1800a
Graeme22 <gholliday2@hotmail.com>
2020-10-15 16:37:24 -0700

    clean up heckendorn's code a bit

--- ass3/symbolTable.h
+++ ass3/symbolTable.h
@@ -1,36 +1,11 @@
 #ifndef _SYMBOLTABLE_H_
 #define _SYMBOLTABLE_H_
+
 #include <map>
 #include <vector>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
-
-// // // // // // // // // // // // // // // // // // // // 
-//
-// Some sample void * printing routines.   User shoud supply their own.
-//
-void pointerPrintAddr(void *data);
-void pointerPrintLongInteger(void *data);
-void pointerPrintStr(void *data);
-
-
-// // // // // // // // // // // // // // // // // // // // 
-//
-// Introduction
-//
-// This symbol table library supplies basic insert and lookup for
-// symbols linked to void * pointers of data.  Plenty of room for
-// improvement inlcuding: better debugging setup, passing of refs
-// rather than values and purpose built char * routines, and C support.
-// Warning: lookup will return NULL pointer if key is not in table.
-//    This means the void * cannot have zero as a legal value.
-//
-// A main() is commented out and has testing code in it.
-//
-// Robert Heckendorn   Oct 1, 2020
-//
-
 
 // // // // // // // // // // // // // // // // // // // // 
 //
@@ -42,22 +17,20 @@ class Scope {
 private:
     static bool debugFlg;                      // turn on tedious debugging
     std::string name;                          // name of scope
-    std::map<std::string , void *> symbols;    // use an ordered map (not as fast as unordered)
+    std::map<std::string, void *> symbols;    // use an ordered map (not as fast as unordered)
 
 public:
-    Scope(std::string newname);
+    Scope(std::string);
     ~Scope();
     std::string scopeName();                   // returns name of scope
-    void debug(bool state);                    // sets the debug flag to new state
+    void debug(bool);                    // sets the debug flag to new state
     void print(void (*printData)(void *));     // prints the table using the supplied function to print the void *
-    void applyToAll(void (*action)(std::string , void *));  // applies func to all symbol/data pairs 
-    bool insert(std::string sym, void *ptr);   // inserts a new ptr associated with symbol sym 
+    void applyToAll(void (*action)(std::string, void *));  // applies func to all symbol/data pairs 
+    bool insert(std::string, void *);   // inserts a new ptr associated with symbol sym 
                                                // returns false if already defined
-    void *lookup(std::string sym);             // returns the ptr associated with sym
+    void *lookup(std::string);             // returns the ptr associated with sym
                                                // returns NULL if symbol not found
 };
-
-
 
 // // // // // // // // // // // // // // // // // // // // 
 //
@@ -74,18 +47,18 @@ private:
 
 public:
     SymbolTable();
-    void debug(bool state);                          // sets the debug flags
+    void debug(bool);                                // sets the debug flags
     int depth();                                     // what is the depth of the scope stack?
     void print(void (*printData)(void *));           // print all scopes using data printing function
-    void enter(std::string name);                    // enter a scope with given name
+    void enter(std::string);                    // enter a scope with given name
     void leave();                                    // leave a scope (not allowed to leave global)
-    void *lookup(std::string sym);                   // returns ptr associated with sym anywhere in symbol table
+    void *lookup(std::string);                   // returns ptr associated with sym anywhere in symbol table
                                                      // returns NULL if symbol not found
-    void *lookupGlobal(std::string sym);             // returns ptr associated with sym in globals
+    void *lookupGlobal(std::string);             // returns ptr associated with sym in globals
                                                      // returns NULL if symbol not found
-    bool insert(std::string sym, void *ptr);         // inserts new ptr associated with symbol sym in current scope
+    bool insert(std::string, void *);         // inserts new ptr associated with symbol sym in current scope
                                                      // returns false if already defined
-    bool insertGlobal(std::string sym, void *ptr);   // inserts a new ptr associated with symbol sym 
+    bool insertGlobal(std::string, void *);   // inserts a new ptr associated with symbol sym 
                                                      // returns false if already defined
     void applyToAll(void (*action)(std::string , void *));        // apply func to all symbol/data pairs in local scope
     void applyToAllGlobal(void (*action)(std::string , void *));  // apply func to all symbol/data pairs in global scope
